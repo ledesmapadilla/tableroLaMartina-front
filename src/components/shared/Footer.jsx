6e8a5af338491d20a58d4629febc45e89a8f4096
IgnacioLP <ignaciolp@lepa.com.ar>
2026-07-13 21:24:30 -0300

    Cambia el guión largo por uno normal en el footer

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="bg-dark text-white text-center py-2">
       <small>
-        © {new Date().getFullYear()} La Martina — Tablero de Control - Ignacio
+        © {new Date().getFullYear()} La Martina - Tablero de Control - Ignacio
         LP
       </small>
     </footer>
